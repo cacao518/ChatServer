43dbcf6f8375843eea52cc6c0cef72971b8494e3
gift_games <qudwns518@naver.com>
2021-02-26 02:32:37 +0900

    최종

--- ChatServer/config.h
+++ ChatServer/config.h
@@ -63,7 +63,7 @@ struct ErrorUtil{
 			NULL, WSAGetLastError(),
 			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 			(LPTSTR)&lpMsgBuf, 0, NULL);
-		MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
+		//MessageBox(NULL, (LPCTSTR)lpMsgBuf, msg, MB_ICONERROR);
 		LocalFree(lpMsgBuf);
 		exit(1);
 	}
